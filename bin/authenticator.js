f6e3adc7f18a0511952b7ff1b1d0914e8d0114ae
徐承 <xucheng.pc@alibaba-inc.com>
2018-11-29 22:54:37 +0800

    common params

--- bin/authenticator.js
+++ bin/authenticator.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+const request = require('../utils/request')
+const Nonce = require('../utils/nonce')
+const baseUrl = 'https://alidns.aliyuncs.com'
+
+const nonceGenerator = Nonce(9)
+const nonce = nonceGenerator()
+
+const commonParams = {
+  Format: 'JSON',
+  Version: '2015-01-09', 
+  AccessKeyId: 'LTAIoYy0HlYDqLRQ',
+  Signature: 
+  SignatureMethod: 'HMAC-SHA1',
+  Timestamp: (new Date()).toISOString(),
+  SignatureVersion: '1.0',
+  SignatureNonce: nonce
+}
